ef5e84aff3c28bbde42c7dce4559ead83319b31f
Emmanuel Lemontzis <lemontzis.e@gmail.com>
2021-02-11 23:06:15 +0100

    add in model unvalidated list of edges and validated list of edges + concept of DAG or Freefrom graph

--- skynet_one/Program.fs
+++ skynet_one/Program.fs
@@ -13,7 +13,8 @@ open GraphAdapters
 let init () =
     let gr =
         read readNbrEdge readEdge
-        |> Reader.run TestScenario0     
+        |> Reader.run TestScenario2     
+        |> cleanUp
 
     let asGraph = function | AsAdjGraph g -> g
     let asAdjList = function | AsAdjList g -> g
@@ -27,15 +28,17 @@ let nicePrint pr =
     |> List.iter (printfn "\tfrom %i")
 
 
-// [<EntryPoint>]
-// let main argv =
+[<EntryPoint>]
+let main argv =
 
+    let grid = TestData.makeGrid 10 |> Seq.toList 
+
+
+    let myG = init() 
+
+    ()
 
 //     bf myGraph 0 
 //     |> Seq.toList
 //     |> Seq.iter nicePrint
-
-
-
-
     0 // return an integer exit code
